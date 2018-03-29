0897adb6b8d78ce7c1baa951929606051e3676ad
Thor Wolpert <thor@wolpert.ca>
2018-03-28 22:10:15 -0700

    testing secrets

--- api/__init__.py
+++ api/__init__.py
@@ -1,13 +1,16 @@
 import os
 import logging
 from flask import Flask
+from config import Config
+
 application = Flask(__name__)
+application.config.from_object(Config)
 
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
 @application.route("/")
 def hello():
-    logging.info('Database: %s', os.getenv('DATABASE_URL','db url missing'))
+    logging.info('Database: %s', Config.SQLALCHEMY_DATABASE_URI)
     return "<h1'>Hello Test!</h1>"
 
 if __name__ == "__main__":
--- config.py
+++ config.py
@@ -1,7 +1,21 @@
 import os
 
-workers = int(os.environ.get('GUNICORN_PROCESSES', '3'))
-threads = int(os.environ.get('GUNICORN_THREADS', '1'))
 
-forwarded_allow_ips = '*'
-secure_scheme_headers = { 'X-Forwarded-Proto': 'https' }
+class Config(object):
+    SECRET_KEY = os.getenv('API_SERVER_SECRET_KEY')
+
+    PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+
+    # POSTGRESQL
+    DB_USER = os.getenv('DATABASE_USERNAME', '')
+    DB_PASSWORD = os.getenv('DATABASE_PASSWORD','')
+    DB_NAME = os.getenv('DATABASE_NAME','')
+    DB_HOST = os.getenv('DATABASE_HOST','')
+    DB_PORT = os.getenv('DATABASE_PORT','')
+    SQLALCHEMY_DATABASE_URI = 'postgresql://{user}:{password}@{host}:{port}/{name}'.format(
+         user=DB_USER,
+         password=DB_PASSWORD,
+         host=DB_HOST,
+         port=DB_PORT,
+         name=DB_NAME,
+    )
