9628d5a11fe2b1c9306a48f92ecae1cc978cd0fd
Thor Wolpert <thor@wolpert.ca>
2018-03-28 21:08:43 -0700

    test env vars in OS

--- api/__init__.py
+++ api/__init__.py
@@ -1,8 +1,13 @@
+import os
+import logging
 from flask import Flask
 application = Flask(__name__)
 
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+
 @application.route("/")
 def hello():
+    logging.info('Database: %s', os.getenv('DATABASE_URL','db url missing'))
     return "<h1'>Hello Test!</h1>"
 
 if __name__ == "__main__":
