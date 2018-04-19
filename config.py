f76d827829422e7178c95d95c8779b909a57902b
Thor Wolpert <thor@wolpert.ca>
2018-04-19 14:30:13 -0700

    move client_secrets to a mountable point

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ class Config(object):
     # OIDC
     SECRET_KEY = os.getenv('SECRET_KEY','')
     OIDC_OPENID_REALM = os.getenv('OIDC_OPENID_REALM','')
-    OIDC_CLIENT_SECRETS = 'client_secrets.json'
+    OIDC_CLIENT_SECRETS = 'client_secrets/client_secrets.json'
     OIDC_USER_INFO_ENABLED = True
     OIDC_SCOPES = ['openid', 'email', 'profile']
     TESTING = True,
